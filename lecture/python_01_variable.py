58b7c347def9f98ce9b67de752c73187ddf33e29
LimDBi <144079364+LimDBi@users.noreply.github.com>
2023-09-08 12:51:38 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(comment): 단순 메모, 개발 실행 x
+
+# 출력 함수(print)
+# - ()안에 값을 추력
+# - 변수 값 확인 용도 또는 메시지 출력 용도
+print("Hello Python")
+
+# 문자열 타입(String Type)
+# - Python: '', "" -> String
+# - C, JAVA: ''(Char), ""(String)
