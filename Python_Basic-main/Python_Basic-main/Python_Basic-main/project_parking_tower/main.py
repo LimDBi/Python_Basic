b3ec709aac4c57bf85a50f04a4c757fd9e3084c1
LimDBi <144079364+LimDBi@users.noreply.github.com>
2023-10-27 11:28:21 +0900

    Update main.py

--- Python_Basic-main/Python_Basic-main/Python_Basic-main/project_parking_tower/main.py
+++ Python_Basic-main/Python_Basic-main/Python_Basic-main/project_parking_tower/main.py
@@ -47,15 +47,27 @@ while True:
                     car_cnt += 1
                     break
         else:
-            print("MSG: 입고가 불가능합니다.")   # 입고 불가
+            print("MSG: 만차입니다. 다음에 이용해주세요.")   # 입고 불가
     elif select_num == 2:   #출고
-        # 1. 차량번호
-        # 2. 주차타워 차량번호 체크
-        # 2-1 차가 없으면 다시 입력받음
-        # 2-2 차가 있으면 출고(tower의 값을 ""으로 바꾸고 car_cnt를 -1해야 됨
+        # 1. 차량번호 입력
+        car_num = input(">>출고: ")
+        # 2. 주차타워 차량번호 체크 (입고 된 차량인지)
+        if car_num in tower:    #True -> 차량이 존재
+            for i, car in enumerate(tower):
+                if car == car_num:  #출고
+                    tower[i] = ""   #타워에서 차량 제거
+                    car_cnt -= 1    #현재 주차 대수 동기화
+                    break
+        else:   # False -> 차량 X
+            print("MSG: 해당 번호로 입고된 차량이 없습니다.")
+        # 3-1. 있으면 -> 출고(tower 해당 차량 제거, car_cnt-1)
+        # 3-2. 없으면 -> "입고된 차량 x"
         pass
     elif select_num == 3:   #조회
-        pass
+        print("== 주차 타워 현황")
+        for i in range(len(tower)-1, -1, -1):
+            print(f"> {i+1}층 {tower[i]}")
     elif select_num == 4:   #종료
         print("MSG: 프로그램을 종료합니다.")
         exit()
+
