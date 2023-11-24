9d66b36dbd681bf91902e0e2272d74cf90df3985
LimDBi <144079364+LimDBi@users.noreply.github.com>
2023-11-24 12:38:45 +0900

    Add files via upload

--- Python_Basic-main/Python_Basic-main/Python_Basic-main/project_daum_movie/main.py
+++ Python_Basic-main/Python_Basic-main/Python_Basic-main/project_daum_movie/main.py
@@ -1,3 +1,10 @@
+# TODO: 1분전 -> 날짜 계산해서 저장(?시간전 완료)
+#       "조금전"
+
+# 다음 영화 DATE → 조금전, ?분전, ?시간전, 2023.11.24 11:09
+# TODO: 스케줄러 등록 하루에 1번 수집(낮12시)
+#       → 중복 방지를 위해서(DB에 저장된 리뷰의 마지막 날짜보다 큰 애들만 수집)
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -10,26 +17,29 @@ import re
 import math
 from bs4 import BeautifulSoup
 
+from db.movie_dao import add_review
 # pip install webdriver_manager
 # pip install selenium
 
 # Selenium + BeautifulSoup4
-#   - Selenium: 전체소스코드 가져오기(+동적으로 페이지 조작)
-#   - BeautifulSoup4: 필요한 데이터만 Select
+#  - Selenium: 전체소스코드 가져오기(+동적으로 페이지 조작)
+#  - BeautifulSoup4: 필요한 데이터만 Select
 
 # **Selenium
-#   - 웹 브라우저 검사(Test) 도구 -> 데이터 수집
-#   - 전용 브라우저(크롬, 파이어폭스)를 동작
-#   - 전용 브라우저 Open -> 작업 -> 브라우저 Close(Default)
+#   - 웹브라우저 검사(Test) 도구 → 데이터 수집
+#   - 전용 브라우저(크롬, 파이어폭스, 등등)로 동작
+#   - 전용 브라우저 Open → 작업 → 브라우저 Close(Default)
 
-# **Selenium
-# 1. 직접 다운로드해서 사용
-#   url: https://sites.google.com/chromium.org/driver/
-# 2. 실시간 다운로드 후 사용
+# ** Selenium 사용방법 2가지
+#  1.직접 다운로드(크롬 브라우저)해서 사용
+#    url: https://sites.google.com/chromium.org/driver/
+
+#  2.실시간 다운로드 후 사용
 options = Options()
 # Selenium이 작업 완료 후에 전용 브라우저 종료 X
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=options)
 
 # URL 접속
 url = "https://movie.daum.net/moviedb/grade?movieId=169137"
@@ -43,20 +53,20 @@ doc = BeautifulSoup(doc_html, "html.parser")
 
 # 영화 제목 수집
 movie_title = doc.select("span.txt_tit")[0].get_text()
-print(movie_title)
+print(f"영화제목: {movie_title}")
+
 
 
 
 total_review_cnt = doc.select("span.txt_netizen")[0].get_text()
-#(187명) 에서 숫자만 추출 방법
-#   1. 문자열 슬라이싱 방법
+# (187명) 에서 숫자만 추출 방법
+#   1.문자열 슬라이싱 방법
 # print(total_review_cnt[1:-2])
-#   2. 정규식 사용한 방법
-num_review = int(re.sub(r"[^~0-9]", "", total_review_cnt))      # 숫자만 뽑는 정규식
-# 187 = 최초(10), 버튼 1개(30개)
-# 올림((187 - 10) / 30) = math.ceil((num_review - 10) / 30)
-click_cnt = math.ceil((num_review - 10) / 30)
+#   2.정규식 사용한 방법
+num_review = int(re.sub(r"[^~0-9]", "", total_review_cnt))
 
+# 187 = 최초(10), 버튼1개(30개)
+click_cnt = math.ceil((num_review - 10) / 30)
 
 for i in range(click_cnt):
     # "평점 더보기" 버튼 클릭
@@ -68,38 +78,41 @@ doc_html = driver.page_source
 doc = BeautifulSoup(doc_html, "html.parser")
 review_list = doc.select("ul.list_comment > li")
 
-print(len(review_list))
+print(f"전체리뷰: {len(review_list)}")
 
 for item in review_list:
     print("=" * 100)
-    review_score = item.select("div.ratings")[0].get_text()       #점이 2개면 클래스가 2개. 점은 하나만 있어야 함.
+    review_score = item.select("div.ratings")[0].get_text()
     print(f"  - 평점: {review_score}")
     review_content = item.select("p.desc_txt")[0].get_text().strip()
-    # strip()은 좌우여백 제거 -> 웬만하면 문자열 읽어들일 때 작성
-    review_content = re.sub("\n", "", review_content)   # 리뷰가 여러 줄일 경우 탭 맞추려고 작성.
     # \n : 한 줄 개행
-    # 수집한 리뷰 개행 -> 문자열 안에 \n 포함
-    # 정규식 이용: 'review_content에서 \n을 만나면 공백으로 바꾸세요'
+    # 수집한 리뷰 개행 → 문자열 안에 \n 포함
+    review_content = re.sub("\n", "", review_content)
     print(f"  - 리뷰: {review_content}")
-    review_writer = item.select("a.link_nick > span")[1].get_text()  #  [댓글 작성자, 작성자, 댓글 모아보기]
-    #닉네임처럼 클래스가 없는 경우 (span 이라는 태그 이름으로는 가져오면 안됨.)매우매우중요
+    review_writer = item.select("a.link_nick > span")[1].get_text()  # [댓글 작성자, 작성자, 댓글 모아보기]
     print(f"  - 작성자: {review_writer}")
-    # print(len(review_writer)) -> 출력 3 1번째 인덱스 출력하면 되겠구나.
 
-    # 24시간 이내에 작성된 글은 날짜 -> 예: 21시간 전, 17시간 전
-    # 실제 날짜 표기법 -> 2023. 11. 17. 12: 15
-    # 21시간 전 -> 2023. 11. 17. 12: 15
-
+    # 24시간 이내에 작성된 글은 날짜 → 예: 21시간전, 17시간전
+    # 실제 날짜 표기법 → 2023. 11. 17. 12:15
+    # 표기법: 21시간전 → 2023. 11. 17. 12:15
     review_date = item.select("span.txt_date")[0].get_text()
-
-    # 21시간 전으로 잘못 뜨는 경우를 어떤 조건을 주면 찾을 수 있을까?
-    #   = review_date -> 17시간 전 or 2023. 11. 17. 12: 17
+    #  = review_date → 17시간전 or 2023. 11. 17. 12:17
     if len(review_date) < 7:
-        # 현재시간 - 17시간
-        # 숫자만 뽑도록 정규식 이용: ex) 17시간 전 -> 숫자만 추출: 17
+        # 예) 17시간전 → 숫자만 추출: 17
         reg_hour = int(re.sub(r"[^~0-9]", "", review_date))
-        # 예) 현재시간(2023.11.17 12: 29) - 18 = 2023-11-16 18:29:23.232500
+        # 예) 현재시간(2023.11.17 12:29) - 18 = 2023-11-16 18:29:23.232500
         review_date = datetime.now() - timedelta(hours=reg_hour)
-        # 예) 2023-11-16 18:29:23.232500 -> 2023. 11. 16. 18:29
+        # 예) 2023-11-16 18:29:23.232500 → 2023. 11. 16. 18:29
         review_date = review_date.strftime("%Y. %m. %d. %H:%M")
     print(f"  - 날짜: {review_date}")
+
+    # MariaDB에 저장
+    #  1) DB에 보낼 데이터 만들기
+    data = {
+        "title": movie_title,
+        "review": review_content,
+        "score": review_score,
+        "writer": review_writer,
+        "reg_date": review_date
+    }
+    add_review(data)
